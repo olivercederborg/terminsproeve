58de36270009dd0901c0a3aa1a729c29aaaacd2a
Oliver Cederborg <hi@olivercederborg.com>
2022-02-15 16:34:16 +0100

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ['./app/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'light-gray': '#eaeaea',
+				'light-pink': '#E1A1E9',
+				'dark-purple': '#5E2E53',
+			},
+		},
+	},
+	plugins: [],
+}
