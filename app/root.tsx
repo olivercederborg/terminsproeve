69fd3be7c26ab17bd83dfb0e6dcad963071d46cd
Oliver Cederborg <hi@olivercederborg.com>
2022-02-17 23:04:37 +0100

    feat(login): add user context

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,19 @@
-import { Links, LinksFunction, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix'
-import type { MetaFunction } from 'remix'
+import type { ReactNode } from 'react'
+import {
+	json,
+	Links,
+	LinksFunction,
+	LiveReload,
+	LoaderFunction,
+	Meta,
+	MetaFunction,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
+} from 'remix'
+import { getSession } from '~/features/login/utils/sessions.server'
+import { UserProvider } from '~/features/login/utils/userContext'
 import styles from './styles/tailwind.css'
 
 export const links: LinksFunction = () => {
@@ -25,21 +39,49 @@ export const meta: MetaFunction = () => {
 	return { title: 'New Remix App' }
 }
 
-export default function App() {
+export const loader: LoaderFunction = async ({ request }) => {
+	const session = await getSession(request.headers.get('Cookie'))
+	const authToken = session.get('access_token')
+	const userId = session.get('user_id')
+
+	if (session.has('access_token')) {
+		const user = await fetch(`http://localhost:4000/api/v1/users/${userId}`, {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${authToken}`,
+			},
+		})
+
+		return json({ user: await user.json() })
+	}
+	return json({ user: null })
+}
+
+const Document = ({ children }: { children: ReactNode }) => (
+	<html lang='en'>
+		<head>
+			<meta charSet='utf-8' />
+			<meta name='viewport' content='width=device-width,initial-scale=1' />
+			<Meta />
+			<Links />
+		</head>
+		<body className='font-standard'>
+			{children}
+			<ScrollRestoration />
+			<Scripts />
+			{process.env.NODE_ENV === 'development' && <LiveReload />}
+		</body>
+	</html>
+)
+
+const App = () => {
+	const user = useLoaderData()
 	return (
-		<html lang='en'>
-			<head>
-				<meta charSet='utf-8' />
-				<meta name='viewport' content='width=device-width,initial-scale=1' />
-				<Meta />
-				<Links />
-			</head>
-			<body className='font-standard'>
+		<UserProvider currentUser={user}>
+			<Document>
 				<Outlet />
-				<ScrollRestoration />
-				<Scripts />
-				{process.env.NODE_ENV === 'development' && <LiveReload />}
-			</body>
-		</html>
+			</Document>
+		</UserProvider>
 	)
 }
+export default App
